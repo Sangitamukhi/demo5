c374870123f4ba077a89a47ed752bf07f3fce241
Sangitamukhi <sangitamukhi9583@gmail.com>
2023-05-19 09:46:00 +0530

    my first commit

---
+++ src/demo1/Assignment.java
@@ -0,0 +1,17 @@
+package demo1;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println(a);   // simple assignment 
+		
+		a+=10; //compound (a=a+10)
+		System.out.println(a);
+		
+		a-=10; //(a=a-10)
+		System.out.println(a);
+	}
+
+}
